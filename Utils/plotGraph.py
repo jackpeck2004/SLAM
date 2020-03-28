ed943ed44cd5077094f1cfc3070c4f89d7674d0d
Giacomo Pasin <giacomo.pasin@gmail.com>
2020-03-28 11:43:34 +0100

    modified

--- Utils/plotGraph.py
+++ Utils/plotGraph.py
@@ -1,19 +1,2 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-def plotGraph(xArr, yArr, frameCount):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    frameCounter = []
-    
-    for i in range(frameCount):
-        frameCounter.append(i)
-
-    for x,y,z in zip(xArr, yArr, frameCounter):
-        ax.scatter(x, z, y)
-
-    plt.show()
-
-    # print(frameCounter)
-
+def plotGraph():
+    print("need to plot graph")
